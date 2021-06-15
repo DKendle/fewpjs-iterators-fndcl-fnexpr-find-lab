ddc426a85597b37e174c2ee25c68d657f82ec4c7
DKendle <dina.kendle@gmail.com>
2021-06-14 20:15:07 -0400

    commit tree 023fd4feecf640d5125f72b07cd2af81ca6cad0d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(array) {
+   let win = array.find(function(obj){
+       return obj.result === "W"
+   })
+   if (win){
+       return win.year
+   } else{
+       return win
+   }
+}
